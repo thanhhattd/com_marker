ddeda964d07a31a825c1cee3544fbfd5a8b06170
thanhhattd <thanhha.humg@gmail.com>
2023-04-04 17:06:51 +0900

    add site folder and some files

---
+++ site/marker.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @version     1.0.2
+ * @package     com_marker
+ * @copyright   Copyright (C) 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * @author      Thanh Hà Nguyễn <thanhha.humg@gmail.com> - http://www.facebook.com/thanhhattd
+ */
+
+defined('_JEXEC') or die;
+
+// Include dependancies
+jimport('joomla.application.component.controller');
+
+// Execute the task.
+$controller	= JControllerLegacy::getInstance('Marker');
+$controller->execute(JFactory::getApplication()->input->get('task'));
+$controller->redirect();
